cc2a636fa944dbe574ad3f9a222ba92fe359156b
davidegrigioni <dav.grigioni@gmail.com>
2023-09-12 23:32:06 +0200

    added color lore

--- src/main/java/cc/davyy/ddapi/utils/chat/ColorUtils.java
+++ src/main/java/cc/davyy/ddapi/utils/chat/ColorUtils.java
@@ -3,10 +3,22 @@ package cc.davyy.ddapi.utils.chat;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ColorUtils {
 
     private ColorUtils() {}
 
     public static Component colorize(String message) { return MiniMessage.miniMessage().deserialize(message); }
+
+    public static List<Component> colorizeLore(String loreString) {
+        List<Component> coloredLore = new ArrayList<>();
+        String[] loreLines = loreString.split("\n");
+        for (String line : loreLines) {
+            coloredLore.add(ColorUtils.colorize(line));
+        }
+        return coloredLore;
+    }
 
 }
